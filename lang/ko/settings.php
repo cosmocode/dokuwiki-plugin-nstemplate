10e4e180cc3c119bc358a97e8220610903392580
Myeongjin <aranet100@gmail.com>
2016-04-20 22:22:12 +0900

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * korean language file for nstemplate plugin
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['nstemplate'] = '간단한 이름공간 템플릿 이름 (새 문서와 같은 이름공간일 때에만 사용됨)';
+$lang['inheritednstemplate'] = '상속되는 이름공간 템플릿 이름 (새 문서와 같거나 높은 이름공간일 때 일치)';
+
+
+
+//Setup VIM: ex: et ts=4 :
